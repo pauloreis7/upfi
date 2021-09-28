e0ffd7e62ab6b7014ab8874f52a180c13f13ef2a
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-09-28 11:50:23 -0300

    Build Homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,32 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface FetchImagesPageResponse {
+  after: string;
+  data: Image[];
+}
+
 export default function Home(): JSX.Element {
+  async function fetchImagesPage({
+    pageParam = null,
+  }): Promise<FetchImagesPageResponse> {
+    const { data } = await api.get('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+
+    return data;
+  }
+
   const {
     data,
     isLoading,
@@ -16,20 +41,23 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImagesPage, {
+    getNextPageParam: lastPage => lastPage?.after ?? null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.flatMap(image => {
+      return image.data.flat();
+    });
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading && !isError) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (!isLoading && isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +65,15 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button
+            isLoading={isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
